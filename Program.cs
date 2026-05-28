bc4e27d00915575e752d1a5d79e2a6df17a5ff0f
Rafael Santos <rafa.05125@gmail.com>
2026-05-28 19:34:56 -0300

    Generating models with Ollama

--- Program.cs
+++ Program.cs
@@ -1,1 +1,27 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using OllamaSharp;
+
+var uri = new Uri("http://localhost:11434");
+var ollama = new OllamaApiClient(uri);
+
+var models = await ollama.ListLocalModelsAsync();
+foreach (var model in models)
+{
+    Console.WriteLine(model.Name);
+}
+
+ollama.SelectedModel = "phi3:latest";
+
+var chat = new Chat(ollama);
+
+var prompt = "You are a astronomy specialist, answer in just one sentence";
+
+prompt += Environment.NewLine;
+prompt += Console.ReadLine();
+
+await foreach (var answer in chat.SendAsync(prompt))
+{
+    Console.Write(answer);
+}
+
+Console.WriteLine();
+Console.WriteLine("--");
